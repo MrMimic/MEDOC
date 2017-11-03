4d151e468e33de54693131922eaad41356a1ea55
MrMimic <emeric.dynomant@gmail.com>
2017-11-03 12:49:14 +0100

    Code v2

--- lib_medline/F_medline_get_dict.py
+++
@@ -1,9 +0,0 @@
-
-
-import re
-import time
-from bs4 import BeautifulSoup
-
-''' Step F: Create a dictionnary with data to INSERT for every article '''
-
-
--- lib_medline/H_remove_file.py
+++
@@ -1,5 +0,0 @@
-
-import os
-import re
-
-''' Step 8: remove file and add file_name to a list to ignore this file next time '''
