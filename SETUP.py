6189083deed66473bc1339bf0917bf3c9dff5b43
MrMimic <emeric.dynomant@gmail.com>
2017-11-14 15:05:00 +0100

    Update SETUP.py

--- SETUP.py
+++ SETUP.py
@@ -30,16 +30,3 @@ except:
 	print('sudo pip3 install bs4')
 	
 print('sudo pip3 install -U lxml')
-
-''' Cythonizer '''
-
-from distutils.core import setup
-from Cython.Build import cythonize
-import os
-
-setup(
-	ext_modules = cythonize('lib_medline/python_functions/*.py'),
-)
-
-os.system('mv *.so lib_medline/')
-os.system('mv lib_medline/python_functions/*.c lib_medline/')
